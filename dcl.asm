1c2e077e1b29e875592b0175ee2d0c8ee398112b
Karol Soczewica <ks394468@students.mimuw.edu.pl>
2020-03-29 19:21:03 +0200

    śmiga tylko styl do poprawy

--- dcl.asm
+++ dcl.asm
@@ -1,12 +1,18 @@
-SYS_READ    equ 0
-SYS_WRITE   equ 1
-SYS_EXIT    equ 60
-STDIN       equ 0
-STDOUT      equ 1
-ARGC        equ 5
-LOWER_BOUND equ 49 
-UPPER_BOUND equ 90
-BUFFER_SIZE equ 4096
+SYS_READ       equ 0
+SYS_WRITE      equ 1
+SYS_EXIT       equ 60
+STDIN          equ 0
+STDOUT         equ 1
+ARGC           equ 5
+LOWER_BOUND    equ 49 
+UPPER_BOUND    equ 90
+BUFFER_SIZE    equ 4096
+PERM_LENGTH    equ 42
+PIVOT_POINT_1  equ 27 ; 'L' - '1'
+PIVOT_POINT_2  equ 33 ; 'R' - '1'
+PIVOT_POINT_3  equ 35 ; 'T' - '1'
+CYLINDER_BOUND equ 41 ; Największa poprawna wartość bębenka.
+REAL_PERM_LEN  equ 43 ; Długość permutacji razem ze znakiem końca napisu.
 
 ; Wykonanie programu zaczyna się od etyiety _start.
 global _start
@@ -31,22 +37,22 @@ global _start
 %endmacro
 
 ; Sprawdza, czy 'r' osiągnęło punkt obrotowy.
-%macro CHECK_CYCLE_POINTS 0
+%macro CHECK_PIVOT_POINTS 0
   mov r8d, r14d ; Wartość bębenka L.
   inc r8d
-  cmp r8d, 41
+  cmp r8d, CYLINDER_BOUND
   cmova r8d, r9d ; Jeśli wyszedł poza zakres, przypisz wartość 0.
-  cmp r15d, 27
+  cmp r15d, PIVOT_POINT_1
   cmove r14d, r8d ; Obróć bębenek L, jeśli bębenek R osiągnął pozycję 'L'.
-  cmp r15d, 33
+  cmp r15d, PIVOT_POINT_2
   cmove r14d, r8d ; Obróć bębenek L, jeśli bębenek R osiągnął pozycję 'R'.
-  cmp r15d, 35
+  cmp r15d, PIVOT_POINT_3
   cmove r14d, r8d ; Obróć bębenek L, jeśli bębenek R osiągnął pozycję 'T'.
 %endmacro
 
 ; Szyfruje bufor.
 %macro CYPHER 0
-  xor ebx, ebx
+  xor ebx, ebx   ; Licznik zaszyfrowanych znaków.
   xor r9d, r9d   ; Potrzebujemy wartości 0 na rejestrze, gdy bębenek się przekręci.
 %%buff_loop:
   cmp rbx, rax
@@ -54,27 +60,27 @@ global _start
   movzx ebp, byte [buffer + ebx] ; Kolejny znak do zaszyfrowania.
   call check_sign
   inc r15d  ; Obracanie bębenka R.
-  cmp r15d, 41
+  cmp r15d, CYLINDER_BOUND
   cmova r15d, r9d ; Przypisz bębenkowi R wartość 0, jeśli wyszedł poza zakres.
-  CHECK_CYCLE_POINTS
+  CHECK_PIVOT_POINTS
   mov r12d, r15d  ; Wartość bębenka R.
   call q_shift
   mov r13, [rsp + 8 * 3] ; Adres permutacji R.
-  movzx ebp, byte [r13 + rbp - '1']
+  movzx ebp, byte [r13 + rbp - '1'] ; Szyfrowanie przy użyciu permutacji R.
   call q_shift_rev
   mov r12d, r14d ; Wartość bębenka L.
   call q_shift
   mov r13, [rsp + 8 * 2] ; Adres permutacji L.
-  movzx ebp, byte [r13 + rbp - '1']
+  movzx ebp, byte [r13 + rbp - '1'] ; Szyfrowanie przy użyciu permutacji L.
   call q_shift_rev
   mov r13, [rsp + 8 * 4] ; Adres permutacji T.
-  movzx ebp, byte [r13 + rbp - '1']
+  movzx ebp, byte [r13 + rbp - '1'] ; Szyfrowanie przy użyciu permutacji T.
   call q_shift
-  movzx ebp, byte [rev_L + rbp - '1']
+  movzx ebp, byte [rev_L + rbp - '1'] ; Szyfrowanie przy użyciu odwrotności permutacji L.
   call q_shift_rev
   mov r12d, r15d ; Wartość bębenka R.
   call q_shift
-  movzx ebp, byte [rev_R + rbp - '1']
+  movzx ebp, byte [rev_R + rbp - '1'] ; Szyfrowanie przy użyciu odwrotności permutacji R.
   call q_shift_rev
   mov byte [buffer + ebx], bpl ; Zapisz zaszyfrowany znak w bufferze.
   inc ebx
@@ -101,26 +107,26 @@ global _start
 
 section .bss
   buffer: resb BUFFER_SIZE
-  rev_L:  resb 42
-  rev_R:  resb 42
-  rev_T:  resb 42
+  rev_L:  resb PERM_LENGTH
+  rev_R:  resb PERM_LENGTH
+  rev_T:  resb PERM_LENGTH
 
 section .text
-q_shift:
+q_shift: ; Wykonuje cykliczne przesunięcie znaku w rejestrze ebp "w przód".
   add ebp, r12d ; Dodaj wartość bębenka L lub R do szyfrowanego znaku.
   mov ecx, ebp
-  sub ecx, 42  ; Odejmij 'Z' i dodaj '0' = odejmij 42.
+  sub ecx, PERM_LENGTH  ; Odejmij 'Z' i dodaj '0' = odejmij 42.
   cmp ebp, UPPER_BOUND
   cmova ebp, ecx ; Przepisz nową wartość, gdy szyfrowany znak wyszedł poza skalę.
   ret
-q_shift_rev:
+q_shift_rev: ; Wykonuje cykliczne przesunięcie znaku w rejestrze ebp "w tył".
   sub ebp, r12d ; Odejmij wartość bębenka L lub R od szyfrowanego znaku.
   mov ecx, ebp
-  add ecx, 42  ; Dodaj 'Z' i odejmij '0' = dodaj 42.
+  add ecx, PERM_LENGTH  ; Dodaj 'Z' i odejmij '0' = dodaj 42.
   cmp ebp, LOWER_BOUND
   cmovb ebp, ecx ; Przepisz nową wartość, gdy szyfrowany znak wyszedł poza skalę.
   ret
-reverse_perm: ; Sprawdza poprawność permutacji i zapisuje jej odwrotność.
+reverse_perm: ; Sprawdza poprawność permutacji, której adres jest w r14 i zapisuje jej odwrotność pod adres z r15.
   mov r8d, LOWER_BOUND
 .arg_loop:
   movzx rbp, byte [r14 + r8 - '1']
@@ -134,15 +140,15 @@ reverse_perm: ; Sprawdza poprawność permutacji i zapisuje jej odwrotność.
   jmp .arg_loop
 .end:
   cld
-  xor al, al  ; Szukaj zera.
-  mov ecx, 42 ; Ogranicz przeszukiwanie do 42 znaków.
+  xor al, al   ; Szukaj zera.
+  mov ecx, REAL_PERM_LEN  ; Ogranicz przeszukiwanie do 43 znaków.
   mov rdi, r15 ; Ustaw adres, od którego rozpocząć szukanie.
   repne scasb  ; Szukaj bajtu o wartości 0.
   sub rdi, r15
-  cmp rdi, 42
+  cmp rdi, REAL_PERM_LEN
   jne error_exit ; Permutacja nie ma 42 znaków.
   ret
-check_sign:
+check_sign: ; Sprawdza, czy znak w rejestrze ebp jest dozwolonym znakiem.
   cmp ebp, LOWER_BOUND
   jb error_exit         ; Znak <= '1'.
   cmp ebp, UPPER_BOUND
