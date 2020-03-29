3844dd2d99ab598f0e9e72f0af807f675d7eb1e6
Karol Soczewica <ks394468@students.mimuw.edu.pl>
2020-03-29 01:27:20 +0100

    poprawiona zoptymalizowana

--- dcl.asm
+++ dcl.asm
@@ -1,4 +1,3 @@
-
 SYS_READ    equ 0
 SYS_WRITE   equ 1
 SYS_EXIT    equ 60
@@ -7,6 +6,7 @@ STDOUT      equ 1
 ARGC        equ 5
 LOWER_BOUND equ 49 
 UPPER_BOUND equ 90
+BUFFER_SIZE equ 4096
 
 ; Wykonanie programu zaczyna się od etyiety _start.
 global _start
@@ -26,26 +26,27 @@ global _start
   mov eax, SYS_READ
   mov edi, STDIN
   mov rsi, buffer
-  mov rdx, 4096
+  mov rdx, BUFFER_SIZE
   syscall
 %endmacro
 
+; Sprawdza, czy 'r' osiągnęło punkt obrotowy.
 %macro CHECK_CYCLE_POINTS 0
-  cmp r11b, 27
-  je %%inc_l
-  cmp r11b, 33
-  je %%inc_l
-  cmp r11b, 35
-  je %%inc_l
-  jmp %%exit
-%%inc_l:
-  inc r10b
-  cmp r10b, 41
-  cmova r10d, r9d
-%%exit:
+  mov r8b, r14b 
+  inc r8b
+  cmp r8b, 41
+  cmova r8d, r9d
+  cmp r15b, 27
+  cmove r14d, r8d
+  cmp r15b, 33
+  cmove r14d, r8d
+  cmp r15b, 35
+  cmove r14d, r8d
 %endmacro
 
+; Szyfruje bufor.
 %macro CYPHER 0
+  xor r9, r9
   xor ebx, ebx
 %%buff_loop:
   movzx ebp, byte [buffer + ebx]
@@ -54,23 +55,26 @@ global _start
   cmp bpl, 10
   je %%end_loop
   call check_sign
-  inc r11b
-  cmp r11b, 41
-  cmova r11d, r9d
+  inc r15b
+  cmp r15b, 41
+  cmova r15d, r9d
   CHECK_CYCLE_POINTS
-  mov r12b, r11b
+  mov r12b, r15b
   call q_shift
-  movzx ebp, byte [r14 + rbp - '1']
-  call q_shift_rev
-  mov r12b, r10b
-  call q_shift
+  mov r13, [rsp + 8 * 3]
   movzx ebp, byte [r13 + rbp - '1']
   call q_shift_rev
-  movzx ebp, byte [r15 + rbp - '1']
+  mov r12b, r14b
+  call q_shift
+  mov r13, [rsp + 8 * 2]
+  movzx ebp, byte [r13 + rbp - '1']
+  call q_shift_rev
+  mov r13, [rsp + 8 * 4]
+  movzx ebp, byte [r13 + rbp - '1']
   call q_shift
   movzx ebp, byte [rev_L + rbp - '1']
   call q_shift_rev
-  mov r12b, r11b
+  mov r12b, r15b
   call q_shift
   movzx ebp, byte [rev_R + rbp - '1']
   call q_shift_rev
@@ -81,24 +85,25 @@ global _start
 %%exit:
 %endmacro
 
+; Sprawdza, czy permutacja T jest złożeniem 21 cykli dwuelementowych.
 %macro CHECK_T_PERMUTATION 0
   xor esi, esi
-  mov sil, '1'
+  mov esi, '1'
 %%rev_Loop:
-  cmp sil, 'Z'
+  cmp esi, 'Z'
   ja %%exit
   cmp sil, byte [r14 + rsi - '1']
   je error_exit
   movzx rcx, byte [r14 + rsi - '1']
   cmp sil, byte [r14 + rcx - '1']
   jne error_exit
-  inc sil
+  inc esi
   jmp %%rev_Loop
 %%exit:
 %endmacro
 
 section .bss
-  buffer: resb 4096
+  buffer: resb BUFFER_SIZE
   rev_L:  resb 42
   rev_R:  resb 42
   rev_T:  resb 42
@@ -119,21 +124,21 @@ q_shift_rev:
   cmovb ebp, ecx
   ret
 reverse_perm: ; zmiana
-  mov r8b, LOWER_BOUND
+  mov r8d, LOWER_BOUND
 .arg_loop:
-  mov bpl, byte [r14 + r8 - '1']
-  test bpl, bpl
+  movzx ebp, byte [r14 + r8 - '1']
+  test ebp, ebp ; a moze test bpl, bpl
   jz .end
   call check_sign
-  sub bpl, LOWER_BOUND
+  sub ebp, LOWER_BOUND
   cmp byte [r15 + rbp], 0
   jne error_exit
   mov [r15 + rbp], r8b
-  inc r8b
+  inc r8d
   jmp .arg_loop
 .end:
   cld
-  xor al, al
+  xor al, al  ; Szukaj zera.
   mov ecx, 42
   mov rdi, r15
   repne scasb
@@ -142,14 +147,15 @@ reverse_perm: ; zmiana
   jne error_exit
   ret
 check_sign:
-  cmp bpl, LOWER_BOUND
+  cmp ebp, LOWER_BOUND
   jb error_exit
-  cmp bpl, UPPER_BOUND
+  cmp ebp, UPPER_BOUND
   ja error_exit
   ret
 _start:
-  cmp byte [rsp], ARGC          
-  jne error_exit      ; Błędna liczba parametrów.
+  cmp byte [rsp], ARGC ; Pod rsp mamy wskaźnik na argc.         
+  jne error_exit       ; Błędna liczba parametrów.
+  xor r8, r8
   mov r14, [rsp + 8 * 2]
   lea r15, [rev_L]
   call reverse_perm
@@ -162,23 +168,21 @@ _start:
   CHECK_T_PERMUTATION
 last_arg:
   mov rsi, [rsp + 8 * 5]
-  mov bpl, byte [rsi]
+  movzx ebp, byte [rsi] ; a moze mov bpl, [rsi]????????
   call check_sign
-  mov r10b, bpl
-  mov bpl, [rsi + 1]
+  mov r14d, ebp
+  movzx ebp, byte [rsi + 1] ; tu moze tez???????????
   call check_sign
-  mov r11b, bpl
-  sub r10b, LOWER_BOUND
-  sub r11b, LOWER_BOUND
+  mov r15d, ebp
+  sub r14d, LOWER_BOUND
+  sub r15d, LOWER_BOUND
   cmp byte [rsi + 2], 0
   jne error_exit
-  mov r13, [rsp + 8 * 2] ; wskaźnik na permutację L
-  mov r14, [rsp + 8 * 3] ; wskaźnik na permutację R
-  mov r15, [rsp + 8 * 4] ; wskaźnik na permutację T
 io_loop:
   READ_INPUT
   test rax, rax
   jz exit
+  xor r9d, r9d
   CYPHER
   PRINT buffer, rbx
   jmp io_loop
